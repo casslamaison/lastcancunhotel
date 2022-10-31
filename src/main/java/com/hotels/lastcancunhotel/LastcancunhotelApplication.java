f91d57df9338a38f5c8122474bb1ce4bc37c9f61
Cassiano <lamaison.cassiano@gmail.com>
2022-10-31 18:28:21 -0300

    initializing project

---
+++ src/main/java/com/hotels/lastcancunhotel/LastcancunhotelApplication.java
@@ -0,0 +1,13 @@
+package com.hotels.lastcancunhotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LastcancunhotelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LastcancunhotelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hotels/lastcancunhotel/LastcancunhotelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hotels.lastcancunhotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LastcancunhotelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
